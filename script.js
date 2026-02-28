a103db5d0a9c1ee434bc7cf5a0464774aca351f1
Stas Ovechenko <user975345@gmail.com>
2026-02-28 12:52:31 +0200

    calculator_project_v2.0

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
-// Введення чисел
 const buttonNumber = document.querySelectorAll(".btn_number");
 const inputFirstNumber = document.getElementById("input_first_number");
 const inputSecondNumber = document.getElementById("input_second_number");
-// стан числа
 let state = "first";
-// блок операції після другого числа
 let blockOperation = false;
 
 buttonNumber.forEach(function(btn) {
     btn.addEventListener("click", function() {
-        // введення першого числа
         if (state === "first") {
             inputFirstNumber.textContent += btn.textContent;
             result.textContent = "";
         } else {
             inputSecondNumber.textContent += btn.textContent;
             blockOperation = true;
-        }
+        };
     });
 });
 
-// введення операції
 const buttonOperation = document.querySelectorAll(".btn_operation");   
 const inputOperation = document.getElementById("input_operation");
 
 buttonOperation.forEach(function(btn) {
     btn.addEventListener("click", function() {
-        // якщо перше число є задаємо операцію
         if (inputFirstNumber.textContent === "" || blockOperation) {
             return;
-        }
+        };
         inputOperation.textContent = btn.textContent;
-        // зміна стану для введення другого числа
         state = "second";
     });
 });
 
-// Clear
 document.getElementById("btn_clear").addEventListener("click", function() {
-    // чистка полів
     inputFirstNumber.textContent = "";
     inputSecondNumber.textContent = "";
     inputOperation.textContent = "";
     result.textContent = "";
-    // скидання стану
     state = "first";
     blockOperation = false;
 });
 
-// калькулятор
+document.getElementById("btn_clear_last").addEventListener("click", function() {
+    if (state === "first") {
+        inputFirstNumber.textContent = inputFirstNumber.textContent.slice(0, -1);
+    } else {
+        if (inputSecondNumber.textContent !== "") {
+            inputSecondNumber.textContent = inputSecondNumber.textContent.slice(0, -1);
+            if (inputSecondNumber.textContent === "") {
+                blockOperation = false;
+            };
+        } else if (inputSecondNumber.textContent === "") {
+            inputOperation.textContent = "";
+            state = "first";
+        };
+    };
+});
+
 const result = document.getElementById("input_result");
 
 document.getElementById("btn_result").addEventListener("click",function () {
@@ -66,11 +71,11 @@ document.getElementById("btn_result").addEventListener("click",function () {
         case '*':
             result.textContent = Number(inputFirstNumber.textContent) * Number(inputSecondNumber.textContent);
             break;
-    }
-    // після result очищаємо поля
-    inputFirstNumber.textContent = "";
+    };
+    inputFirstNumber.textContent = result.textContent;
     inputSecondNumber.textContent = "";
     inputOperation.textContent = "";
+    result.textContent = "";
     state = "first";
     blockOperation = false;
-});
+}); 
